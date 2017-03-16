041fbb1e7bef3ac9893936ff8108409826bbea70
Pavel <piv@sitecore.net>
2017-03-16 14:59:18 +0200

    Fix issue 55552 [Chrome] WebEditRibbon flickers on smaller screen sizes/in dialog windows

---
+++ src/Sitecore.Support.55552/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.55552")]
+[assembly: AssemblyProduct("Sitecore.Support.55552")]
+[assembly: ComVisible(false)]
